94d66e0c5614258802526e22088c1320255f2d0e
Martin Benninger <martin.benninger@outlook.com>
2017-12-11 01:14:52 +0100

    Load the template file

--- resources/ui/widget/RtcGitConnector.js
+++ resources/ui/widget/RtcGitConnector.js
@@ -1,14 +1,17 @@
 define([
     "dojo/_base/declare",
     "dijit/_WidgetBase",
-    "dijit/_TemplatedMixin"
-], function(declare, _WidgetBase, _TemplateMixin) {
+    "dijit/_TemplatedMixin",
+    "dojo/text!./templates/RtcGitConnector.html",
+    "dijit/Dialog"
+], function(declare, _WidgetBase, _TemplateMixin, template) {
     return declare([_WidgetBase, _TemplateMixin], {
+        templateString: template,
+
         constructor: function(params) {
             this.workItem = params;
-
-            console.log("constructor params: ", params);
-            alert("Test from constructor");
+            mainDialog.show();
+            console.log("templateString: ", this.templateString);
         }
     });
 });
