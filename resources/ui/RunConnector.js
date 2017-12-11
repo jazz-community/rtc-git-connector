4cc3f870d8b72b869921e253860c2e8e5769de57
Martin Benninger <martin.benninger@outlook.com>
2017-12-11 13:26:36 +0100

    Create a hover view for the widget

--- resources/ui/RunConnector.js
+++ resources/ui/RunConnector.js
@@ -1,44 +1,57 @@
 define([
     "dojo/_base/declare",
-    "./widget/RtcGitConnector"
-], function(declare, RtcGitConnector) {
+    "dijit/focus",
+    "./widget/RtcGitConnector",
+    "../library/ActionNode",
+    "../library/HoverViewWrapper"
+], function(declare, focus, RtcGitConnector, ActionNode, HoverViewWrapper) {
     var cssSelector = "img.button-img";
     return declare("com.siemens.bt.jazz.workitemeditor.rtcGitConnector.ui.RunConnector",
-        com.ibm.team.workitem.web.ui2.internal.action.AbstractAction, {
-            // summary:
-            //  Default constructor called when the WorkItemEditor is instantiated.
-            //  For in-depth information on the creation procedure, see
-            //  WorkItemEditorHeader.js, where all actions are created.
-            //
-            // params: {actionSpec, workingCopy}
-            //  actionSpec corresponds to the action values defined in plugin.xml.
-            //  workingCopy is a reference to the current work item being edited:
-            //  {
-            //      actionSpec: {
-            //          action,
-            //          iconContext,
-            //          iconUri,
-            //          id,
-            //          label,
-            //          parameter
-            //      },
-            //      workingCopy: {}
-            //  }
-            constructor: function(params) {
-            },
+        com.ibm.team.workitem.web.ui2.internal.action.AbstractAction,
+    {
+        // summary:
+        //  Default constructor called when the WorkItemEditor is instantiated.
+        //  For in-depth information on the creation procedure, see
+        //  WorkItemEditorHeader.js, where all actions are created.
+        //
+        // params: {actionSpec, workingCopy}
+        //  actionSpec corresponds to the action values defined in plugin.xml.
+        //  workingCopy is a reference to the current work item being edited:
+        //  {
+        //      actionSpec: {
+        //          action,
+        //          iconContext,
+        //          iconUri,
+        //          id,
+        //          label,
+        //          parameter
+        //      },
+        //      workingCopy: {}
+        //  }
+        constructor: function (params) {
+            var nodeLabel = params.actionSpec.label;
+            this._buttonNode = new ActionNode(cssSelector, nodeLabel);
+        },
 
-            isEnabled: function(params) {
-                var workingCopy = params.workingCopy || params;
-                return !workingCopy.isChanged() && !workingCopy.isNewWorkItem();
-            },
+        isEnabled: function (params) {
+            var workingCopy = params.workingCopy || params;
+            return !workingCopy.isChanged() && !workingCopy.isNewWorkItem();
+        },
 
-            //  summary:
-            //      Is run when the action button in the WorkItemEditor view is clicked.
-            //  params: {actionSpec, workingCopy}
-            //      Same as the params passed to the constructor.
-            run: function(params) {
-                var workingCopy = params.workingCopy || params;
-                new RtcGitConnector(workingCopy);
-            }
+        //  summary:
+        //      Is run when the action button in the WorkItemEditor view is clicked.
+        //  params: {actionSpec, workingCopy}
+        //      Same as the params passed to the constructor.
+        run: function (params) {
+            var widget = this.makeWidget(params);
+            var hoverViewWrapper = new HoverViewWrapper(this._buttonNode.getPosition(), widget);
+            focus.focus(hoverViewWrapper.getDomNode());
+        },
+
+        makeWidget: function (params) {
+            var minWidth = this._buttonNode.calculateMinWidth();
+            var workingCopy = params.workingCopy || params;
+            return new RtcGitConnector(minWidth, workingCopy);
+        }
     });
 });
