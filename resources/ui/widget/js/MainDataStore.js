6fcdbc1d3b9653f0a80241b2e33f31251e3cac26
Martin Benninger <martin.benninger@outlook.com>
2017-12-12 18:25:49 +0100

    Add a property for the selected git repository

--- resources/ui/widget/js/MainDataStore.js
+++ resources/ui/widget/js/MainDataStore.js
@@ -1,12 +1,15 @@
 define([
     "dojo/_base/declare",
+    "dojo/Stateful",
     "dojox/mvc/StatefulArray"
-], function (declare, StatefulArray) {
+], function (declare, Stateful, StatefulArray) {
     return declare(null, {
         registeredGitRepositories: null,
+        selectedGitRepository: null,
 
         constructor: function () {
             this.registeredGitRepositories = new StatefulArray([]);
+            this.selectedGitRepository = new Stateful();
         }
     });
 });
