6b612f06bec05a737872840256189460287eb4de
Martin Benninger <martin.benninger@outlook.com>
2017-12-12 11:02:33 +0100

    Add initial MainDataStore

---
+++ resources/ui/widget/js/MainDataStore.js
@@ -0,0 +1,13 @@
+define([
+    "dojo/_base/declare",
+    "dojo/store/Memory",
+    "dojo/store/Observable"
+], function (declare, Memory, Observable) {
+    return declare(null, {
+        registeredGitRepositoryStore: null,
+
+        constructor: function () {
+            this.registeredGitRepositoryStore = new Observable(new Memory({ idProperty: "key", data: [] }));
+        }
+    });
+});
