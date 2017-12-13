14b7e499286077931bd22dd3bb5ac5376498a90e
Martin Benninger <martin.benninger@outlook.com>
2017-12-13 15:04:26 +0100

    Add the allRegisteredGitRepositories url

--- resources/ui/widget/js/JazzRestService.js
+++ resources/ui/widget/js/JazzRestService.js
@@ -2,7 +2,12 @@ define([
     "dojo/_base/declare"
 ], function (declare) {
     return declare(null, {
+        allRegisteredGitRepositoriesUrl: null,
+
         constructor: function () {
+            this.allRegisteredGitRepositoriesUrl =
+                    net.jazz.ajax._contextRoot +
+                    "/service/com.ibm.team.git.common.internal.IGitRepositoryRegistrationRestService/allRegisteredGitRepositories";
         }
     });
 });
