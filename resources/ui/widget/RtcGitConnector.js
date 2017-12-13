b835cf770a36e4879a0edf28087daac08e1429f4
Martin Benninger <martin.benninger@outlook.com>
2017-12-14 00:30:59 +0100

    Remove the unnecessary ids and destroyWidget calls

--- resources/ui/widget/RtcGitConnector.js
+++ resources/ui/widget/RtcGitConnector.js
@@ -35,9 +35,7 @@ define([
 
             this.mainDialog.onHide = function () {
                 // Destroy all dialogs and remove them from the dom
-                self.destroyWidgetById("com-siemens-bt-jazz-workitemeditor-rtcGitConnector-ui-widget-selectRegisteredGitRepository");
                 self.destroyWidgetById("mainLayoutMyDialog");
-                self.destroyWidgetById("com-siemens-bt-jazz-workitemeditor-rtcGitConnector-ui-widget-mainLayout");
                 this.destroyRecursive(false);
 
                 // Destroy the data store
