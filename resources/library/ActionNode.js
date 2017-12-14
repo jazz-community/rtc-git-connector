97ceb3b0580d983935a472a92ea691fa895a3db2
Martin Benninger <martin.benninger@outlook.com>
2017-12-14 21:30:16 +0100

    Fixed formatting

--- resources/library/ActionNode.js
+++ resources/library/ActionNode.js
@@ -10,7 +10,7 @@
 //		Alt text to search for. This is necessary for being able to select the
 //		dom node created by a custom action. As we are limited to the node
 //		information that we can declare in plugin.xml, we sadly have to select
-//		a node based on the alt-text. 
+//		a node based on the alt-text.
 //		<b>Make sure this is unique in the dom!</b>
 
 define([
@@ -18,22 +18,20 @@ define([
         "dojo/query",
         "dojo/window"
 ], function(declare, query, window) {
-
 	var ViewUtils = com.ibm.team.rtc.foundation.web.ui.views.ViewUtils;
 
 	return declare(null, {
-
 		constructor: function(cssSelector, nodeLabel) {
 			this._queryString = cssSelector + "[alt=" + nodeLabel + "]";
 		},
-		
+
 //	summary:
 //		Returns the current position of the ActionNode in the dom. Uses the
 //		getDomNodePosition function provided by Jazz ViewUtils instead of the
 //		native dojo/dom-geometry::position() function because of the included
 //		vertical offset. Will throw warnings because of deprecated function
 //		usage in ViewUtils :-/
-//		
+//
 //	returns: Position {x, y}
 //		Current dom coordinates.
 //
@@ -44,12 +42,12 @@ define([
 //
 //	description:
 //		this is fairly hacky because it uses the label text passed to the
-//		toolbar action for finding the correct dom-node. However, for 
-//		custom web-ui actions, there seems to be no other way to imitate 
-//		the behaviour of other widgets shown in hoverviews, such as 
+//		toolbar action for finding the correct dom-node. However, for
+//		custom web-ui actions, there seems to be no other way to imitate
+//		the behavior of other widgets shown in HoverViews, such as
 //		CreateWorkItemCopyAction. It will only work on the last item
 //		added to the dom, as this is the required functionality for our
-//		use case. This is because, when using the browser history 
+//		use case. This is because, when using the browser history
 //		functions, the dom is conserved and not rebuilt. Therefore, we
 //		need to work with the most recently added node with the selector
 //		+ label combination.
@@ -58,7 +56,7 @@ define([
 			var node = nodeList.pop();
 			return ViewUtils.getDomNodePosition(node);
 		},
-		
+
 		calculateMinWidth: function() {
 			var currentPosition = this.getPosition();
 			var viewPort = window.getBox();
