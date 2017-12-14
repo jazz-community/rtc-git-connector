23b5a1f0f64d59020d565cca27fc0208ad5ec373
Martin Benninger <martin.benninger@outlook.com>
2017-12-14 22:58:54 +0100

    Document the MainDataStore

--- resources/ui/widget/js/MainDataStore.js
+++ resources/ui/widget/js/MainDataStore.js
@@ -19,7 +19,15 @@ define([
             this.selectedRepositorySettings = new Stateful(this.selectedRepositorySettings);
         }
     });
+
+    // Returns an instance so that you don't need to instantiate this class.
+    // It's functions can be called directly after importing. Example:
+    //      MainDataStore.getInstance();
+    //      MainDataStore.destroyInstance();
+    //
+    // This is basically a singleton that can be asked to use a new instance when needed
     return new function () {
+        // Gets the existing instance or creates one if none exists (singleton)
         this.getInstance = function () {
             if (!_instance) {
                 _instance = new MainDataStore();
@@ -28,6 +36,8 @@ define([
             return _instance;
         };
 
+        // Destroys the existing instance. It doesn't matter if none exists.
+        // This causes the next call to getInstance to create a new instance
         this.destroyInstance = function () {
             _instance = null;
         };
