c22480377d20dcee74af21746977771321731e88
Martin Benninger <martin.benninger@outlook.com>
2017-12-18 16:26:56 +0100

    Add a webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const packageJson = require('./package.json');
+const JazzUpdateSitePlugin = require('jazz-update-site-webpack-plugin');
+
+module.exports = (env) => {
+   const version = (typeof env !== 'undefined' && env.buildUUID) || packageJson.version;
+	const config = {
+		entry: {
+			app: './src/RtcGitConnectorModules.js',
+		},
+		output: {
+			libraryTarget: 'var',
+			library: 'com_siemens_bt_jazz_rtcgitconnector_modules',
+			filename: './resources/dist/modules-bundle.js',
+		},
+
+		plugins: [
+			new JazzUpdateSitePlugin({
+				appType: 'ccm',
+				projectId: 'com.siemens.bt.jazz.workitemeditor.rtcGitConnector',
+				acceptGlobPattern: [
+				   'resources/**',
+				   'META-INF/**',
+				   'plugin.xml',
+				],
+				projectInfo: {
+				   author: packageJson.author,
+				   copyright: packageJson.author,
+				   description: packageJson.description,
+				   license: packageJson.license,
+				   version: version,
+				},
+			}),
+		],
+	};
+	return config;
+};
