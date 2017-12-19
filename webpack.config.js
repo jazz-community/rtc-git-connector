6ae32e4354be350b021df51ac449820f5c9f1db3
MartinBenninger <martin.benninger@outlook.com>
2017-12-19 10:17:26 +0100

    Fix whitespace

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,44 @@ const packageJson = require('./package.json');
 const JazzUpdateSitePlugin = require('jazz-update-site-webpack-plugin');
 
 module.exports = (env) => {
-   const version = (typeof env !== 'undefined' && env.buildUUID) || packageJson.version;
-	const config = {
+    const version = (typeof env !== 'undefined' && env.buildUUID) || packageJson.version;
+    const config = {
         node : {
             fs : 'empty',
             net : 'empty',
             tls : 'empty'
         },
 
-		entry: {
-			app: './src/RtcGitConnectorModules.js',
+        entry: {
+            app: './src/RtcGitConnectorModules.js',
         },
 
-		output: {
-			libraryTarget: 'var',
-			library: 'com_siemens_bt_jazz_rtcgitconnector_modules',
-			filename: './resources/dist/modules-bundle.js',
-		},
+        output: {
+            libraryTarget: 'var',
+            library: 'com_siemens_bt_jazz_rtcgitconnector_modules',
+            filename: './resources/dist/modules-bundle.js',
+        },
 
-		plugins: [
-			new JazzUpdateSitePlugin({
-				appType: 'ccm',
-				projectId: 'com.siemens.bt.jazz.workitemeditor.rtcGitConnector',
-				acceptGlobPattern: [
-				   'resources/**',
-				   'META-INF/**',
-				   'plugin.xml',
-				],
-				projectInfo: {
-				   author: packageJson.author,
-				   copyright: packageJson.author,
-				   description: packageJson.description,
-				   license: packageJson.license,
-				   version: version,
-				},
+        plugins: [
+            new JazzUpdateSitePlugin({
+                appType: 'ccm',
+                projectId: 'com.siemens.bt.jazz.workitemeditor.rtcGitConnector',
+                acceptGlobPattern: [
+                   'resources/**',
+                   'META-INF/**',
+                   'plugin.xml',
+                ],
+                projectInfo: {
+                   author: packageJson.author,
+                   copyright: packageJson.author,
+                   description: packageJson.description,
+                   license: packageJson.license,
+                   version: version,
+                },
             }),
 
             new UglifyJsPlugin()
-		]
-	};
-	return config;
+        ]
+    };
+    return config;
 };
