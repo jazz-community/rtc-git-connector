b9babbfbc392675fd2a67fcb16addf12fcb62800
Martin Benninger <martin.benninger@outlook.com>
2017-12-29 14:46:13 +0100

    Add the initial select link type widget

---
+++ resources/ui/widget/js/SelectLinkType.js
@@ -0,0 +1,22 @@
+define([
+    "dojo/_base/declare",
+    "./MainDataStore",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetsInTemplateMixin",
+    "dojo/text!../templates/SelectLinkType.html"
+], function (declare,
+    MainDataStore,
+    _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
+    template) {
+    return declare("com.siemens.bt.jazz.workitemeditor.rtcGitConnector.ui.widget.selectLinkType",
+        [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin],
+    {
+        templateString: template,
+        mainDataStore: null,
+
+        constructor: function () {
+            this.mainDataStore = MainDataStore.getInstance();
+        }
+    });
+});
