7928cdff0e19fc195cd91316973f05df8100d7b3
MartinBenninger <martin.benninger@outlook.com>
2018-01-04 18:12:40 +0100

    Add a basic model

--- resources/ui/widget/js/CommitModel.js
+++ resources/ui/widget/js/CommitModel.js
@@ -2,7 +2,10 @@ define([
     "dojo/_base/declare"
 ], function (declare) {
     return declare(null, {
-        constructor: function () {
-        },
+        sha: null,              // The full sha of the commit
+        message: null,          // The full commit message
+        committerName: null,    // The full name of the committer
+        committerEmail: null,   // The email of the committer
+        committedDate: null     // The date & time when the commit was made
     });
 });
