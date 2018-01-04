5feac887f9b4c03f7953f6e370b69e4c7db2b180
MartinBenninger <martin.benninger@outlook.com>
2018-01-04 18:28:59 +0100

    Provide functions for creating the commit model

--- resources/ui/widget/js/CommitModel.js
+++ resources/ui/widget/js/CommitModel.js
@@ -1,11 +1,23 @@
 define([
     "dojo/_base/declare"
 ], function (declare) {
-    return declare(null, {
+    var CommitModel = declare(null, {
         sha: null,              // The full sha of the commit
         message: null,          // The full commit message
         committerName: null,    // The full name of the committer
         committerEmail: null,   // The email of the committer
         committedDate: null     // The date & time when the commit was made
     });
+
+    return new function () {
+        this.CreateFromGitHubCommit = function (gitHubCommit) {
+            console.log("create from git hub commit: ", gitHubCommit);
+            return new CommitModel();
+        };
+
+        this.CreateFromGitLabCommit = function (gitLabCommit) {
+            console.log("create from git lab commit: ", gitLabCommit);
+            return new CommitModel();
+        };
+    };
 });
