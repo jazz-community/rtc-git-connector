3081f0b5bab608e981b7c4bbfbf53d704b43e5de
MartinBenninger <martin.benninger@outlook.com>
2018-01-05 16:09:55 +0100

    Watch the store to know when the commits loaded

--- resources/ui/widget/js/ViewAndSelectCommits.js
+++ resources/ui/widget/js/ViewAndSelectCommits.js
@@ -1,23 +1,39 @@
 define([
     "dojo/_base/declare",
+    "./DataStores/MainDataStore",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     "dojo/text!../templates/ViewAndSelectCommits.html"
 ], function (declare,
+    MainDataStore,
     _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
     template) {
     return declare("com.siemens.bt.jazz.workitemeditor.rtcGitConnector.ui.widget.viewAndSelectCommits",
         [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin],
     {
         templateString: template,
+        mainDataStore: null,
 
         constructor: function () {
-            console.log("ViewAndSelectCommits constructor");
+            this.mainDataStore = MainDataStore.getInstance();
         },
 
         startup: function () {
-            console.log("ViewAndSelectCommits startup");
+            this.watchDataStore();
+        },
+
+        watchDataStore: function () {
+            // Watch the store to know when the commits finished loading
+            this.mainDataStore.selectedRepositorySettings.watch("commitsLoaded", function (name, oldValue, value) {
+                if (value) {
+                    // Commits finished loading, update the view...
+                    console.log("commits finished loading");
+                } else {
+                    // Commits are not loaded, reinitialize the view (loading...)
+                    console.log("commits not loaded");
+                }
+            });
         }
     });
 });
