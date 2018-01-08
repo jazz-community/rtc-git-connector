ea338568fa51bab7d682680f56e77f62c8fd93a6
Martin Benninger <martin.benninger@outlook.com>
2018-01-09 00:32:28 +0100

    Watch the commitsToLink in the store

--- resources/ui/widget/js/ViewCommitsToLink.js
+++ resources/ui/widget/js/ViewCommitsToLink.js
@@ -1,23 +1,42 @@
 define([
     "dojo/_base/declare",
+    "dojo/dom-style",
+    "./DataStores/MainDataStore",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     "dojo/text!../templates/ViewCommitsToLink.html"
-], function (declare,
+], function (declare, domStyle,
+    MainDataStore,
     _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
     template) {
     return declare("com.siemens.bt.jazz.workitemeditor.rtcGitConnector.ui.widget.viewCommitsToLink",
         [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin],
     {
         templateString: template,
+        mainDataStore: null,
 
         constructor: function () {
-            console.log("ViewCommitsToLink constructor");
+            this.mainDataStore = MainDataStore.getInstance();
         },
 
         startup: function () {
-            console.log("ViewCommitsToLink startup");
+            this.watchDataStore();
+        },
+
+        watchDataStore: function () {
+            var self = this;
+
+            // React when commits are added or removed from the commits to link list
+            this.mainDataStore.selectedRepositoryData.commitsToLink.watchElements(function () {
+                if (self.mainDataStore.selectedRepositoryData.commitsToLink.length > 0) {
+                    // show commits to link list
+                    domStyle.set("viewCommitsToLinkContainer", "display", "block");
+                } else {
+                    // hide commits to link list
+                    domStyle.set("viewCommitsToLinkContainer", "display", "none");
+                }
+            });
         }
     });
 });
