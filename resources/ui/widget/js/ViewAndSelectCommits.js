4724e2c33dd254e4bbb38f94ff7fe9e871050cab
MartinBenninger <martin.benninger@outlook.com>
2018-01-08 15:20:47 +0100

    Create a view list for the commits

--- resources/ui/widget/js/ViewAndSelectCommits.js
+++ resources/ui/widget/js/ViewAndSelectCommits.js
@@ -1,11 +1,13 @@
 define([
     "dojo/_base/declare",
+    "dojo/_base/array",
+    "dojo/_base/lang",
     "./DataStores/MainDataStore",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     "dojo/text!../templates/ViewAndSelectCommits.html"
-], function (declare,
+], function (declare, array, lang,
     MainDataStore,
     _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
     template) {
@@ -14,12 +16,14 @@ define([
     {
         templateString: template,
         mainDataStore: null,
+        viewCommits: null,
 
         constructor: function () {
             this.mainDataStore = MainDataStore.getInstance();
         },
 
         startup: function () {
+            this.initializeViewCommitsList();
             this.watchDataStore();
         },
 
@@ -31,9 +35,11 @@ define([
                 if (value) {
                     // Commits finished loading, update the view...
                     console.log("commits finished loading");
+                    self.setViewCommitsListFromStore();
                 } else {
                     // Commits are not loaded, reinitialize the view (loading...)
                     console.log("commits not loaded");
+                    self.initializeViewCommitsList();
                 }
             });
 
@@ -43,8 +49,39 @@ define([
                 if (self.mainDataStore.selectedRepositorySettings.get("commitsLoaded")) {
                     // Update the local list of commits...
                     console.log("commits list changed");
+                    self.setViewCommitsListFromStore();
                 }
             });
+        },
+
+        initializeViewCommitsList: function () {
+            this.viewCommits = [{
+                message: "Loading...",
+                disabled: true
+            }];
+
+            // Draw view...
+            console.log("draw view commits ", this.viewCommits);
+        },
+
+        setViewCommitsListFromStore: function () {
+            // Clone the store array
+            this.viewCommits = lang.clone(this.mainDataStore.selectedRepositoryData.commits);
+
+            if (this.viewCommits.length < 1) {
+                this.viewCommits = [{
+                    message: "No commits found",
+                    disabled: true
+                }];
+            } else {
+                array.forEach(this.viewCommits, function (commit) {
+                    commit.selected = false;
+                    commit.disabled = commit.alreadyLinked;
+                });
+            }
+
+            // Draw view...
+            console.log("draw view commits ", this.viewCommits);
         }
     });
 });
