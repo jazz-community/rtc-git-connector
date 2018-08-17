de65773185c9b562274d95656907d4e8009ca59a
MartinBenninger <martin.benninger@outlook.com>
2018-08-17 16:13:08 +0200

    Update some comments

--- resources/ui/widget/js/HandlebarsTemplates/TemplateService.js
+++ resources/ui/widget/js/HandlebarsTemplates/TemplateService.js
@@ -9,8 +9,8 @@ define([
             this.handlebars = com_siemens_bt_jazz_rtcgitconnector_modules.Handlebars;
             this.turndownService = new com_siemens_bt_jazz_rtcgitconnector_modules.TurndownService();
 
-            // Get rid of this and use the option {escapeMarkdown: false}
-            // as soon as it's available in a turndown release
+            // Override the escape method so that markdown is not escaped
+            // when converting HTML to Markdown
             this.turndownService.escape = function (input) {
                 return input;
             };
@@ -27,6 +27,9 @@ define([
             var self = this;
 
             this.handlebars.registerHelper('turndown', function (inputString) {
+                // Convert HTML to Markdown. First replace non-breaking spaces with normal ones.
+                // The HTML editor in Jazz creates non-breaking spaces when there are multiple spaces in a row.
+                // For the Markdown formatting to work correctly, these need to be normal spaces.
                 return inputString ? self.turndownService.turndown(inputString.replace(/&nbsp;/g, ' ')) : '';
             });
         }
