d9809e4a0769977846d8119ee17f2b9b2993f4bb
MartinBenninger <martin.benninger@outlook.com>
2018-08-30 12:20:59 +0200

    Add some attributes with setters to ListItem.js

--- resources/ui/widget/components/ListItem/ListItem.js
+++ resources/ui/widget/components/ListItem/ListItem.js
@@ -1,15 +1,42 @@
 define([
     "dojo/_base/declare",
+    "dojo/dom-class",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!./ListItem.html",
     "jazz/css!./ListItem.css"
-], function (declare,
+], function (declare, domClass,
     _WidgetBase, _TemplatedMixin,
     template) {
     return declare("com.siemens.bt.jazz.workitemeditor.rtcGitConnector.ui.widget.listItem",
         [_WidgetBase, _TemplatedMixin,],
     {
-        templateString: template
+        templateString: template,
+
+        selected: false,
+        _setSelectedAttr: function (selected) {
+            if (selected) {
+                domClass.add(this.listItem, "selected");
+            } else {
+                domClass.remove(this.listItem, "selected");
+            }
+
+            this._set("selected", selected);
+        },
+
+        itemId: "",
+        _setItemIdAttr: { node: "listItem", type: "attribute", attribute: "data-item-id" },
+
+        title: "",
+        _setTitleAttr: { node: "firstLine", type: "innerHTML" },
+
+        details: "",
+        _setDetailsAttr: { node: "secondLine", type: "innerHTML" },
+
+        constructor: function (itemId) {
+            this.itemId = itemId;
+
+            console.log("listItem this: ", this);
+        }
     });
 });
