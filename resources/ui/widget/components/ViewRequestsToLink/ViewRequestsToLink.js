60a1c3490407106c3b570010b1f95a669e70ce6f
MartinBenninger <martin.benninger@outlook.com>
2018-09-03 17:31:51 +0200

    Use the LinkItem widget in ViewRequestsToLink

--- resources/ui/widget/components/ViewRequestsToLink/ViewRequestsToLink.js
+++ resources/ui/widget/components/ViewRequestsToLink/ViewRequestsToLink.js
@@ -1,18 +1,18 @@
 define([
     "dojo/_base/declare",
     "dojo/_base/array",
+    "dojo/_base/lang",
     "dojo/dom-construct",
     "dojo/dom-style",
-    "dojo/on",
-    "dojo/query",
     "../../services/MainDataStore",
     "../../js/ViewHelper",
+    "../ListItem/ListItem",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     "dojo/text!./ViewRequestsToLink.html"
-], function (declare, array, domConstruct, domStyle, on, query,
-    MainDataStore, ViewHelper,
+], function (declare, array, lang, domConstruct, domStyle,
+    MainDataStore, ViewHelper, ListItem,
     _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
     template) {
     return declare("com.siemens.bt.jazz.workitemeditor.rtcGitConnector.ui.widget.viewRequestsToLink",
@@ -53,45 +53,41 @@ define([
         // Draw the requests to link list in the view
         drawRequestsToLink: function (requestsToLink) {
             var self = this;
-            var requestsListNode = query("#viewRequestsToLinkContainer .rtcGitConnectorViewItemsToLinkList")[0];
-            domConstruct.empty(requestsListNode);
+            domConstruct.empty(this.listItemsContainer);
 
             array.forEach(requestsToLink, function (request) {
-                var requestListItem = domConstruct.create("div", {
-                    "class": "rtcGitConnectorViewAndSelectListItem itemToLink",
-                    "data-request-id": request.id
-                }, requestsListNode);
+                var listItem = new ListItem(request.id);
+                listItem.set("title", request.title);
+                listItem.set("details", ViewHelper.GetIssueOrRequestDateString(request));
+                listItem.set("buttonType", "trash");
+                listItem.set("notClickable", true);
 
-                on(requestListItem, "click", function (event) {
-                    var requestId = this.getAttribute("data-request-id");
+                listItem.onButtonClick = lang.hitch(self, self.listItemButtonClick);
 
-                    if (ViewHelper.IsNodeInClass(event.target, "rtcGitConnectorViewAndSelectListItemButton")) {
-                        // Remove the request with the specified id from the requests to link list in store and add to the requests list
-                        if (requestId) {
-                            var selectedRequest = null;
-
-                            for (var i = self.mainDataStore.selectedRepositoryData.requestsToLink.length - 1; i >= 0; i--) {
-                                if (self.mainDataStore.selectedRepositoryData.requestsToLink[i].id == requestId) {
-                                    selectedRequest = self.mainDataStore.selectedRepositoryData.requestsToLink.splice(i, 1)[0];
-                                    break;
-                                }
-                            }
-
-                            if (selectedRequest && !self.mainDataStore.selectedRepositoryData.requests.find(function (request) {
-                                return request.id == selectedRequest.id;
-                            })) {
-                                self.mainDataStore.selectedRepositoryData.requests.push(selectedRequest);
-                            }
-                        }
-                    }
-                });
-
-                var secondLine = ViewHelper.GetIssueOrRequestDateString(request);
-                ViewHelper.DrawListItem(requestListItem, request.title, secondLine, "removeButton", "trash");
+                request.listItem = listItem;
+                domConstruct.place(listItem.domNode, self.listItemsContainer);
             });
 
             // Get the mainDialog and resize to fit the new content
             ViewHelper.ResizeMainDialog();
+        },
+
+        // Remove the request with the specified id from the requests to link list in store and add to the requests list
+        listItemButtonClick: function (itemId) {
+            var selectedRequest = null;
+
+            for (var i = this.mainDataStore.selectedRepositoryData.requestsToLink.length - 1; i >= 0; i--) {
+                if (this.mainDataStore.selectedRepositoryData.requestsToLink[i].id == itemId) {
+                    selectedRequest = this.mainDataStore.selectedRepositoryData.requestsToLink.splice(i, 1)[0];
+                    break;
+                }
+            }
+
+            if (selectedRequest && !this.mainDataStore.selectedRepositoryData.requests.find(function (request) {
+                return request.id == selectedRequest.id;
+            })) {
+                this.mainDataStore.selectedRepositoryData.requests.push(selectedRequest);
+            }
         }
     });
 });
