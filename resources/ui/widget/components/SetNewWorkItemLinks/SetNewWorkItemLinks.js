7bb24af0121fc00bfcd562c1db1513dca96550e7
Martin Benninger <martin.benninger@outlook.com>
2019-04-02 13:29:54 +0200

    Show the parent work item below the add parent button

--- resources/ui/widget/components/SetNewWorkItemLinks/SetNewWorkItemLinks.js
+++ resources/ui/widget/components/SetNewWorkItemLinks/SetNewWorkItemLinks.js
@@ -11,10 +11,14 @@ define([
     "dojo/text!./SetNewWorkItemLinks.html",
     "jazz/css!./SetNewWorkItemLinks.css",
     "com.ibm.jdojox.util.ArrayList",
+    "com.ibm.jdojox.util.JDojoX",
     "com.ibm.team.rtc.foundation.web.model.Bindable",
     "com.ibm.team.rtc.foundation.web.model.BindableList",
+    "com.ibm.team.rtc.foundation.web.model.Label",
+    "com.ibm.team.rtc.foundation.web.model.ListBindings",
     "com.ibm.team.rtc.foundation.web.ui.util.Sprites",
     "com.ibm.team.rtc.foundation.web.ui.views.controller.ActionDropdown",
+    "com.ibm.team.rtc.foundation.web.ui.views.controller.ArtifactMultiList",
     "com.ibm.team.workitem.web.model.links.WorkItemEndpoints",
     "com.ibm.team.workitem.web.ui.internal.view.editor.presentations.nonattribute.links.LinksDialogLauncher"
 ], function (declare, lang, domConstruct, domStyle, string,
@@ -23,10 +27,14 @@ define([
     _WidgetBase, _TemplatedMixin,
     template) {
     var ArrayList = com.ibm.jdojox.util.ArrayList;
+    var JDojoX = com.ibm.jdojox.util.JDojoX;
     var Bindable = com.ibm.team.rtc.foundation.web.model.Bindable;
     var BindableList = com.ibm.team.rtc.foundation.web.model.BindableList;
+    var Label = com.ibm.team.rtc.foundation.web.model.Label;
+    var ListBindings = com.ibm.team.rtc.foundation.web.model.ListBindings;
     var Sprites = com.ibm.team.rtc.foundation.web.ui.util.Sprites;
     var ActionDropdown = com.ibm.team.rtc.foundation.web.ui.views.controller.ActionDropdown;
+    var ArtifactMultiList = com.ibm.team.rtc.foundation.web.ui.views.controller.ArtifactMultiList;
     var WorkItemEndpoints = com.ibm.team.workitem.web.model.links.WorkItemEndpoints;
     var LinksDialogLauncher = com.ibm.team.workitem.web.ui.internal.view.editor.presentations.nonattribute.links.LinksDialogLauncher;
 
@@ -64,35 +72,72 @@ define([
             var actionsMenuDiv = domConstruct.create("div", null, this.linksContainer);
             var actionsMenu = ActionDropdown.create({}, actionsMenuDiv);
 
-            actionsMenu.renderer(lang.hitch(this, function(endPoint) {
+            actionsMenu.renderer(lang.hitch(this, function (endpoint) {
                 return new MenuItem({
-                    label: this._getEndpointLabel(endPoint),
-                    iconClass: Sprites.cssClassName(endPoint.getIcon())
+                    label: this._getEndpointLabel(endpoint),
+                    iconClass: Sprites.cssClassName(endpoint.getIcon())
                 });
             })).values(endpointList).defaultValue(defaultEndpoint).bind();
 
-            actionsMenu.valueChosen.addListener(lang.hitch(this, function(chosenDescriptor) {
+            actionsMenu.valueChosen.addListener(lang.hitch(this, function (chosenDescriptor) {
                 this._launchDialog(workingCopy, workItemReferences, chosenDescriptor);
             }));
 
             domStyle.set(actionsMenu._view._dropdownElement, "float", "none");
             domStyle.set(actionsMenu._view._dropdownElement, "border-right", "none");
             domStyle.set(actionsMenu._view._dropdownArrow, "display", "none");
+
+            // Links pres
+            var listViewDiv = domConstruct.create("div", null, this.linksContainer);
+            var listView = ArtifactMultiList.create(listViewDiv);
+            var endpoints = new BindableList();
+            var headers= new BindableList();
+            var readOnly= new BindableList();
+            var typesMap= new BindableList();
+
+            endpoints.add(parentEndpoint);
+
+            ListBindings.bindWithAdapter(endpoints, headers, function (endpoint) {
+                return new Label(endpoint.getDisplayName()).iconUrl(endpoint.getIcon().toUri());
+            });
+
+            ListBindings.bindWithAdapter(endpoints, readOnly, function (endpoint) {
+                return new Bindable(!endpoint.isUserDeleteable());
+            });
+
+            ListBindings.bindWithAdapter(endpoints, typesMap, function (endpoint) {
+                var result = new BindableList();
+                var references = workItemReferences.getReferences(endpoint);
+
+                ListBindings.bind(references, result);
+
+                result.onListChanged().addListener(function (callbackArg) {
+                    if (!JDojoX.isEqual(result, references) && callbackArg.type === 'remove') {
+                        workItemReferences.remove(endpoint, callbackArg.elements[0]);
+                    }
+                });
+
+                return result;
+            });
+
+            listView.renderer(function (itemReference) {
+                return new Label(itemReference.getComment()).iconUrl(itemReference.getIconUrl()).url(itemReference.getUrl());
+            }).headers(headers).sections(typesMap).readOnly(readOnly).compact(new Bindable(false)).bind();
         },
 
-        _getEndpointLabel: function(endPoint) {
-            return string.substitute(endPoint.isSingleValued() ? "Set ${0}" : "Add ${0}", [endPoint.getDisplayName()]);
+        _getEndpointLabel: function (endpoint) {
+            return string.substitute(endpoint.isSingleValued() ? "Set ${0}" : "Add ${0}", [endpoint.getDisplayName()]);
         },
 
-        _launchDialog: function(workItem, workItemReferences, chosenDescriptor) {
-            LinksDialogLauncher.launchDialog(workItem, chosenDescriptor, function(endPoint) {
+        _launchDialog: function (workItem, workItemReferences, chosenDescriptor) {
+            LinksDialogLauncher.launchDialog(workItem, chosenDescriptor, function (endpoint) {
                 var references = Array.prototype.slice.call(arguments, 1);
 
-                if (endPoint.isSingleValued() && workItemReferences.hasReferences(endPoint)) {
-                    workItemReferences.remove(endPoint, workItemReferences.getReferences(endPoint).at(0));
+                if (endpoint.isSingleValued() && workItemReferences.hasReferences(endpoint)) {
+                    workItemReferences.remove(endpoint, workItemReferences.getReferences(endpoint).at(0));
                 }
 
-                workItemReferences.add.apply(workItemReferences, [endPoint].concat(references));
+                workItemReferences.add.apply(workItemReferences, [endpoint].concat(references));
             });
         }
     });
