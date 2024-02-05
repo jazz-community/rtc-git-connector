b68de1e35b86278541c978f329b192ce19a8c4ff
Martin Benninger <martin.benninger@outlook.com>
2024-02-05 18:20:21 +0100

    Return an instance from the CommitLinkEncoder

--- src/CommitLinkEncoder.js
+++ src/CommitLinkEncoder.js
@@ -1,27 +1,26 @@
-const pako = require("pako");
-const base64js = require("base64-js");
-const Buffer = require("buffer/").Buffer;
+new (function () {
+    const base64js = require("base64-js");
+    const Buffer = require("buffer/").Buffer;
+    const pako = require("pako");
 
-// Replace some characters in the input string (needed due to the strange implementation)
-function base64UrlEncode(string) {
-    string = string.replace(/\+/g, "-");
-    string = string.replace(/\//g, "_");
-    string = string.replace(/=/g, ".");
-    return string;
-}
+    // Replace some characters in the input string (needed due to the strange implementation)
+    function base64UrlEncode(string) {
+        string = string.replace(/\+/g, "-");
+        string = string.replace(/\//g, "_");
+        string = string.replace(/=/g, ".");
+        return string;
+    }
 
-// Replace some characters in the input string (needed due to the strange implementation)
-function base64UrlDecode(string) {
-    string = string.replace(/-/g, "+");
-    string = string.replace(/_/g, "/");
-    string = string.replace(/\./g, "=");
-    return string;
-}
+    // Replace some characters in the input string (needed due to the strange implementation)
+    function base64UrlDecode(string) {
+        string = string.replace(/-/g, "+");
+        string = string.replace(/_/g, "/");
+        string = string.replace(/\./g, "=");
+        return string;
+    }
 
-// Export the functions needed for encoding and decoding data in git commit links
-export function encoder() {
     // Creates an encoded string from a stringified json object
-    this.encode = function encode(value) {
+    this.encode = function (value) {
         const compressed = pako.gzip(Buffer.from(value));
         const encoded = base64js.fromByteArray(compressed);
         const urlEncoded = base64UrlEncode(encoded);
@@ -29,11 +28,11 @@ export function encoder() {
     };
 
     // Creates a stringified json object from the encoded string
-    this.decode = function decode(value) {
+    this.decode = function (value) {
         const urlDecoded = base64UrlDecode(value);
         const compressed = base64js.toByteArray(urlDecoded);
         const deflated = pako.ungzip(compressed);
         const buffer = Buffer.from(deflated);
         return buffer.toString();
     };
-}
+})();
