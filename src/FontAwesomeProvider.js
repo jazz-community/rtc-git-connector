5cfa68bea6133b1cfe9550456d5caf9481f2ae61
Martin Benninger <martin.benninger@outlook.com>
2024-02-05 17:49:58 +0100

    Remove unnecessary define

--- src/FontAwesomeProvider.js
+++ src/FontAwesomeProvider.js
@@ -1,24 +1,21 @@
-define(function () {
-    // Return an instance so that the functions can be used as if they were static
-    return new (function () {
-        this.FontAwesome = require("@fortawesome/fontawesome");
+new (function () {
+    this.FontAwesome = require("@fortawesome/fontawesome");
 
-        const FaCheck = require("@fortawesome/fontawesome-free-solid/faCheck");
-        const FaExclamationTriangle = require("@fortawesome/fontawesome-free-solid/faExclamationTriangle");
-        const FaLink = require("@fortawesome/fontawesome-free-solid/faLink");
-        const FaMinus = require("@fortawesome/fontawesome-free-solid/faMinus");
-        const FaPlus = require("@fortawesome/fontawesome-free-solid/faPlus");
-        const FaSpinner = require("@fortawesome/fontawesome-free-solid/faSpinner");
-        const FaTimes = require("@fortawesome/fontawesome-free-solid/faTimes");
-        const FaTrash = require("@fortawesome/fontawesome-free-solid/faTrash");
+    const FaCheck = require("@fortawesome/fontawesome-free-solid/faCheck");
+    const FaExclamationTriangle = require("@fortawesome/fontawesome-free-solid/faExclamationTriangle");
+    const FaLink = require("@fortawesome/fontawesome-free-solid/faLink");
+    const FaMinus = require("@fortawesome/fontawesome-free-solid/faMinus");
+    const FaPlus = require("@fortawesome/fontawesome-free-solid/faPlus");
+    const FaSpinner = require("@fortawesome/fontawesome-free-solid/faSpinner");
+    const FaTimes = require("@fortawesome/fontawesome-free-solid/faTimes");
+    const FaTrash = require("@fortawesome/fontawesome-free-solid/faTrash");
 
-        this.FontAwesome.library.add(FaCheck);
-        this.FontAwesome.library.add(FaExclamationTriangle);
-        this.FontAwesome.library.add(FaLink);
-        this.FontAwesome.library.add(FaMinus);
-        this.FontAwesome.library.add(FaPlus);
-        this.FontAwesome.library.add(FaSpinner);
-        this.FontAwesome.library.add(FaTimes);
-        this.FontAwesome.library.add(FaTrash);
-    })();
-});
+    this.FontAwesome.library.add(FaCheck);
+    this.FontAwesome.library.add(FaExclamationTriangle);
+    this.FontAwesome.library.add(FaLink);
+    this.FontAwesome.library.add(FaMinus);
+    this.FontAwesome.library.add(FaPlus);
+    this.FontAwesome.library.add(FaSpinner);
+    this.FontAwesome.library.add(FaTimes);
+    this.FontAwesome.library.add(FaTrash);
+})();
