41ce77589fcaef9a8c3dbd77375c42e2ebf6c83c
Martin Benninger <martin.benninger@outlook.com>
2024-02-06 14:12:46 +0100

    Use define in the src js files

--- src/CommitLinkEncoder.js
+++ src/CommitLinkEncoder.js
@@ -1,38 +1,41 @@
-new (function () {
-    const base64js = require("base64-js");
-    const Buffer = require("buffer/").Buffer;
-    const pako = require("pako");
+define(function () {
+    // Return an instance so that the functions can be used as if they were static
+    return new (function () {
+        const base64js = require("base64-js");
+        const Buffer = require("buffer/").Buffer;
+        const pako = require("pako");
 
-    // Replace some characters in the input string (needed due to the strange implementation)
-    function base64UrlEncode(string) {
-        string = string.replace(/\+/g, "-");
-        string = string.replace(/\//g, "_");
-        string = string.replace(/=/g, ".");
-        return string;
-    }
+        // Replace some characters in the input string (needed due to the strange implementation)
+        function base64UrlEncode(string) {
+            string = string.replace(/\+/g, "-");
+            string = string.replace(/\//g, "_");
+            string = string.replace(/=/g, ".");
+            return string;
+        }
 
-    // Replace some characters in the input string (needed due to the strange implementation)
-    function base64UrlDecode(string) {
-        string = string.replace(/-/g, "+");
-        string = string.replace(/_/g, "/");
-        string = string.replace(/\./g, "=");
-        return string;
-    }
+        // Replace some characters in the input string (needed due to the strange implementation)
+        function base64UrlDecode(string) {
+            string = string.replace(/-/g, "+");
+            string = string.replace(/_/g, "/");
+            string = string.replace(/\./g, "=");
+            return string;
+        }
 
-    // Creates an encoded string from a stringified json object
-    this.encode = function (value) {
-        const compressed = pako.gzip(Buffer.from(value));
-        const encoded = base64js.fromByteArray(compressed);
-        const urlEncoded = base64UrlEncode(encoded);
-        return urlEncoded;
-    };
+        // Creates an encoded string from a stringified json object
+        this.encode = function (value) {
+            const compressed = pako.gzip(Buffer.from(value));
+            const encoded = base64js.fromByteArray(compressed);
+            const urlEncoded = base64UrlEncode(encoded);
+            return urlEncoded;
+        };
 
-    // Creates a stringified json object from the encoded string
-    this.decode = function (value) {
-        const urlDecoded = base64UrlDecode(value);
-        const compressed = base64js.toByteArray(urlDecoded);
-        const deflated = pako.ungzip(compressed);
-        const buffer = Buffer.from(deflated);
-        return buffer.toString();
-    };
-})();
+        // Creates a stringified json object from the encoded string
+        this.decode = function (value) {
+            const urlDecoded = base64UrlDecode(value);
+            const compressed = base64js.toByteArray(urlDecoded);
+            const deflated = pako.ungzip(compressed);
+            const buffer = Buffer.from(deflated);
+            return buffer.toString();
+        };
+    })();
+});
