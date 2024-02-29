cd07b389eac09e84cb12d5b29bed2465ae5494d7
Martin Benninger <martin.benninger@outlook.com>
2024-02-29 16:43:23 +0100

    Name the AMD bundles to work with the plainjavascript provider

--- webpack.config.js
+++ webpack.config.js
@@ -3,116 +3,110 @@ const ZipPlugin = require("zip-webpack-plugin");
 const moment = require("moment");
 const packageJson = require("./package.json");
 
-module.exports = (env) => {
-    const timestamp = moment().format("[_]YYYYMMDD[-]HHmm");
-    const version =
-        (typeof env !== "undefined" &&
-            typeof env.buildUUID !== "undefined" &&
-            packageJson.version + "_" + env.buildUUID) ||
-        packageJson.version + timestamp;
-    const config = {
-        target: ["web", "es5"],
-        resolve: {
-            fallback: {
-                "https": false
+const version = packageJson.version + "_" + (process.env.BUILD_TIMESTAMP || moment().format("YYYYMMDD[-]HHmm"));
+const config = {
+    target: ["web", "es5"],
+    resolve: {
+        fallback: {
+            "https": false
+        }
+    },
+    externalsType: "var",
+    externals: {
+        "currencyformatter.js": "currencyformatter.js",
+        "moment/min/moment-with-locales": "moment",
+        "sprintf-js": "sprintf-js"
+    },
+    entry: {
+        CommitLinkEncoder: ["./src/CommitLinkEncoder.js"],
+        FontAwesomeProvider: ["./src/FontAwesomeProvider.js"],
+        ClipboardJS: ["clipboard"],
+        GitHubApi: ["@octokit/rest"],
+        GitLabApiProvider: ["./src/GitLabApiProvider.js"],
+        Handlebars: ["handlebars"],
+        JustHandlebarsHelpers: ["just-handlebars-helpers"],
+        TurndownService: ["turndown"]
+    },
+    output: {
+        library: {
+            type: "amd",
+            name: "com.siemens.bt.jazz.workitemeditor.rtcGitConnector|dist|[name].js"
+        },
+        filename: "[name].js",
+        path: __dirname + "/resources/dist"
+    },
+    optimization: {
+        minimize: false,
+        concatenateModules: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        exclude: /node_modules[\\/]@babel/,
+                        presets: [["@babel/preset-env", { "modules": "amd" }]],
+                        plugins: ["@babel/plugin-transform-runtime"]
+                    }
+                }
             }
-        },
-        externalsType: "var",
-        externals: {
-            "currencyformatter.js": "currencyformatter.js",
-            "moment/min/moment-with-locales": "moment",
-            "sprintf-js": "sprintf-js"
-        },
-        entry: {
-            CommitLinkEncoder: ["./src/CommitLinkEncoder.js"],
-            FontAwesomeProvider: ["./src/FontAwesomeProvider.js"],
-            ClipboardJS: ["clipboard"],
-            GitHubApi: ["@octokit/rest"],
-            GitLabApiProvider: ["./src/GitLabApiProvider.js"],
-            Handlebars: ["handlebars"],
-            JustHandlebarsHelpers: ["just-handlebars-helpers"],
-            TurndownService: ["turndown"]
-        },
-        output: {
-            library: {
-                type: "amd"
+        ]
+    },
+    plugins: [
+        new RemovePlugin({
+            before: {
+                root: __dirname,
+                test: [
+                    {
+                        folder: "./",
+                        method: (filePath) => {
+                            return new RegExp(
+                                /com\.siemens\.bt\.jazz\.workitemeditor\.rtcGitConnector.*\.zip$/,
+                                "i"
+                            ).test(filePath);
+                        }
+                    }
+                ],
+                include: ["./resources/dist"]
             },
-            filename: "[name].js",
-            path: __dirname + "/resources/dist"
-        },
-        optimization: {
-            minimize: false,
-            concatenateModules: false
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            exclude: /node_modules[\\/]@babel/,
-                            presets: [["@babel/preset-env", { "modules": "amd" }]],
-                            plugins: ["@babel/plugin-transform-runtime"]
-                        }
-                    }
-                }
-            ]
-        },
-        plugins: [
-            new RemovePlugin({
-                before: {
-                    root: __dirname,
-                    test: [
-                        {
-                            folder: "./",
-                            method: (filePath) => {
-                                return new RegExp(
-                                    /com\.siemens\.bt\.jazz\.workitemeditor\.rtcGitConnector.*\.zip$/,
-                                    "i"
-                                ).test(filePath);
-                            }
-                        }
-                    ],
-                    include: ["./resources/dist"]
-                },
-                after: {
-                    root: __dirname,
-                    include: ["dist"]
-                }
-            })
-        ]
-    };
-
-    const themeConfig = {
-        target: ["web", "es5"],
-        entry: "./src/theme/customMenuItems.js",
-        output: {
-            path: __dirname + "/dist",
-            filename: "com.siemens.bt.jazz.workitemeditor.rtcGitConnector_theme_" + version + ".js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            exclude: /node_modules[\\/]@babel/,
-                            presets: [["@babel/preset-env", { "modules": "amd" }]],
-                            plugins: ["@babel/plugin-transform-runtime"]
-                        }
-                    }
-                }
-            ]
-        },
-        plugins: [
-            new ZipPlugin({
-                path: __dirname,
-                filename: "com.siemens.bt.jazz.workitemeditor.rtcGitConnector_theme_" + version + ".zip"
-            })
-        ]
-    };
-
-    return [config, themeConfig];
+            after: {
+                root: __dirname,
+                include: ["dist"]
+            }
+        })
+    ]
 };
+
+const themeConfig = {
+    target: ["web", "es5"],
+    entry: "./src/theme/customMenuItems.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "com.siemens.bt.jazz.workitemeditor.rtcGitConnector_theme_" + version + ".js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        exclude: /node_modules[\\/]@babel/,
+                        presets: [["@babel/preset-env", { "modules": "amd" }]],
+                        plugins: ["@babel/plugin-transform-runtime"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ZipPlugin({
+            path: __dirname,
+            filename: "com.siemens.bt.jazz.workitemeditor.rtcGitConnector_theme_" + version + ".zip"
+        })
+    ]
+};
+
+module.exports = [config, themeConfig];
